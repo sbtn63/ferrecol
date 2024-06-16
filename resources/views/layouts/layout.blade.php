bc21226f50a6a3727066b0bed76cb1e7357fb44d
rei <rei@localhost.localdomain>
2024-06-15 22:43:47 -0500

    config assets

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
 
 @yield('content')
+
+
+<script src="{{ asset('js/app.js') }}"></script>
 
 </body>
 </html>
