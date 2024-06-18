f88653dcf2aaf2e1aaad9f380283a74b808fac27
rei <rei@localhost.localdomain>
2024-06-18 00:14:43 -0500

    changes

--- backend/scripts/00-laravel-deploy.sh
+++ backend/scripts/00-laravel-deploy.sh
@@ -4,7 +4,6 @@ composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
 echo "generating application key..."
-php artisan key:generate --show
 
 echo "Caching config..."
 php artisan config:cache
