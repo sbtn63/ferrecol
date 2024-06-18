7eb051ebe6ddd72ae97b5fb146e2d12dd391c294
rei <rei@localhost.localdomain>
2024-06-18 10:41:37 -0500

    changes

--- backend/scripts/00-laravel-deploy.sh
+++ backend/scripts/00-laravel-deploy.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
-
-echo "generating application key..."
-
-echo "Caching config..."
-php artisan config:cache
-
-echo "Caching routes..."
-php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+#echo "Running seeders..."
+#php artisan db:seed
+
+#echo "Running vite..."
+#npm install
+#npm run build
